ccc94c85d179780add8ace5491f223d7e3ca6583
outsider9567 <adanny91113@gmail.com>
2023-09-27 12:04:21 +0800

    0927

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import MyName from './myname'
 import MyButton from './mybutton'
-
+import Click from './click'
+import ProductList from './product/productList'
 export default function Home() {
   return (<div className={styles.main}>
     <h1>Hello</h1>
-    <MyName/>
-    <MyButton/>
-    </div>
-  )
+    <MyName />
+    <MyButton />
+    <MyButton />
+    <Click />
+    <ProductList/>
+  </div>)
 }
---
+++ app/product/productList.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from 'react';
+import { Box, List, ListItem, ListItemText, ListItemButton } from "@mui/material";
+
+
+export default function ProductList() {
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const handleListItemClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        index: number,
+    ) => {
+    setSelectedIndex(index);
+  };
+
+  let products = [
+    { desc: "iPad", price: 20000 },
+    { desc: "iPhone 8", price: 20000 },
+    { desc: "iPhone X", price: 30000 }
+  ];
+  return (
+    <Box sx={{
+      width: '80vw',
+      height: '100vh',
+      color: 'black',
+      textAlign: 'left'
+    }}>
+        
+        <List subheader="Product list" aria-label="product list">
+        {products.map((product, index) =>
+          <ListItem divider key={product.desc}>
+            <ListItemButton
+                selected={selectedIndex === index}
+                onClick={(event) => handleListItemClick(event, index)}>
+            <ListItemText primary={product.desc} secondary={product.price}>
+            </ListItemText>
+            </ListItemButton>
+          </ListItem>)}
+      </List>
+    </Box>
+  );
+}
